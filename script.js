a53a778a399d5e4080d3b65fe07360a89ed291d8
Dauntflash <ndwiga14053.@student.mmarau.ac.ke>
2024-07-22 22:28:12 +0300

    update

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
 function addTask(){
-    var taskInput= document.getElementById('taskInput').value;
+    const task= document.getElementById('taskInput');
+    const taskInput= document.getElementById('taskInput').value;
     if (taskInput.trim() !== ''){
         var taskList = document.getElementById('taskList');
-        var taskItem = document.createElement('li');
-        taskItem.textContent = taskInput;
 
-        var removeButton = document.createElement('button');
-        removeButton.textContent='Done';
-        removeButton.addEventListener('click',function(){
-            taskList.removeChild(taskItem);
-        });
-        taskList.appendChild(taskItem);
-        document.getElementById('taskInput').value = '';
-        taskItem.addEventListener('click', function() {
-            taskList.removeChild(taskItem);
-        });
+        var checker = document.createElement('input');
+        checker.type = 'checkbox';
+        checker.id='checker';
+        checker.onchange = function(){
+            if (this.checked){
+                this.parentElement.style.textDecoration = 'line-through';
+                const parentList= taskItem.parentElement;
+                parentList.appendChild(taskItem);
+            } else {
+                this.parentElement.style.textDecoration = 'none';
+            }
+        }
+
+        var image= document.createElement('img');
+        image.src = 'icons8-trash.svg';
+        image.onclick = function(){
+            this.parentElement.remove();
+        }
+        image.id="delete";
+
+        var taskItem = document.createElement('li');
+        taskItem.appendChild(checker);
+        taskItem.appendChild(document.createTextNode(taskInput));
+        taskItem.appendChild(image);
+
+
+        if (taskList.firstChild){
+            taskList.insertBefore(taskItem, taskList.firstChild);
+        }
+        else{
+            taskList.appendChild(taskItem);
+        }
+
+        task.value = ' ';
+
     } else{
         alert("Please enter a task!");
     }
