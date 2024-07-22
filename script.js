3970de61c0f94da0c96b2f347c2bba231cbe5350
Dauntflash <ndwiga14053.@student.mmarau.ac.ke>
2024-07-22 22:35:24 +0300

    update

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function addTask(){
         }
 
         var image= document.createElement('img');
-        image.src = 'icons8-trash.svg';
+        image.src = 'assets/icons8-trash.svg';
         image.onclick = function(){
             this.parentElement.remove();
         }
